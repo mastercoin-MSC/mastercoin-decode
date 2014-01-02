8025da2dea66b26ecbb282b7eabbd45699dcf091
Maran <maran.hidskes@gmail.com>
2014-01-02 23:37:35 +0100

    Light refactoring and started on Class B simple send implementation

--- simple_send.go
+++ simple_send.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 import "strconv"
+import "strings"
 import "math/big"
 import "encoding/binary"
 
@@ -12,15 +13,19 @@ var log = logging.MustGetLogger("")
 
 func main(){
   logging.SetFormatter(logging.MustStringFormatter("[%{level}] %{message}"))
-  logging.SetLevel(logging.INFO, "")
+  logging.SetLevel(logging.DEBUG, "")
 
   ss := DecodeFromAddress("15NoSD4F1ULYHGW3TK6khe1rLSS2qoysaX")
   ss.Explain()
 
-  a := ss.EncodeToAddress()
+  a := ss.SerializeToAddress()
 
   ss2 := DecodeFromAddress(a)
   ss2.Explain()
+
+  ss2.SerializeToKey()
+}
+type Message struct {
 }
 
 type SimpleSend struct {
@@ -41,36 +46,105 @@ func (ss *SimpleSend) Explain(){
 // 4 = > [0,0,0,0,0,0,0,4]
 func makeBinary(value interface{}) []byte{
   var z []byte
+  var val uint64
 
-  if val, ok := value.(uint32); ok{
-    str := strconv.FormatUint(uint64(val), 10)
+  amount := 4
 
-    number := new(big.Int)
-    number.SetString(str, 10)
-
-    template := []byte{0,0,0,0}
-
-    x := number.Bytes()
-    z = append(template[:(4-len(x))],x...)
-  } else if val, ok := value.(uint64); ok{
-    str := strconv.FormatUint(val, 10)
-
-    number := new(big.Int)
-    number.SetString(str, 10)
-
-    template := []byte{0,0,0,0,0,0,0,0}
-
-    x := number.Bytes()
-    z = append(template[:(8-len(x))],x...)
-  } else {
+  if v, ok := value.(uint64); ok{
+    amount = 8
+    val = v
+  }else if v, ok := value.(uint32); ok{
+    val = uint64(v)
+  }else {
     panic(fmt.Sprintf("makeBinary requires a value that's either a uint32 or an uint64, got: %s ", value))
   }
+
+  str := strconv.FormatUint(val, 10)
+
+  number := new(big.Int)
+  number.SetString(str, 10)
+
+  template := make([]byte, amount)
+
+  x := number.Bytes()
+  z = append(template[:(amount-len(x))],x...)
 
   return z
 }
 
-// Encodes Class A - Simple Sends
-func (ss *SimpleSend) EncodeToAddress() string{
+// Converts a number to a string array
+// i.e. 100,8 => [0,0,0,0,0,1,0,0]
+// i.e. 66 ,4 => [0,0,6,6]
+func makeStringArray(value string, length int) []string{
+  z := make([]string,length)
+  for i, _ := range z {
+    z[i] = "0"
+  }
+
+  pointer := length-len(value)
+  for _, val := range value {
+    z[pointer] = fmt.Sprintf("%c",val)
+    pointer++
+  }
+  return z
+}
+
+// Takes SerializeToKey output and builds a valid, obfuscated public key
+func (ss *SimpleSend) SerializeToCompressedPublicKey(xor_target string) string{
+  return "nothing"
+}
+
+// Encodes as Class B
+// Encodes the data to a format that will be used as Obfuscate source
+
+func (ss *SimpleSend) SerializeToKey() string{
+  log.Info("Encoding data to KEY")
+
+  raw := make([]string, 62)
+  for i, _ := range raw {
+    raw[i] = "0"
+
+    // This is the 'fake' sequence number, which we don't really need for Class B
+    if i == 1{
+      raw[i] = "1"
+    }
+  }
+
+  transaction_type := makeStringArray(strconv.FormatUint(uint64(ss.transaction_type), 16), 8)
+  log.Debug("Transaction type: ",transaction_type)
+
+  currency_id :=  makeStringArray(strconv.FormatUint(uint64(ss.currency_id), 16), 8)
+  log.Debug("Currency ID: ", currency_id)
+
+  amount := makeStringArray(strconv.FormatUint(ss.amount, 16), 16)
+  log.Debug("Amount: ",amount)
+
+  // Start of the data
+  pointer := 2
+
+  // TODO: Perhaps make this a bit DRYer if there is no other way of doing it
+  for _, value := range transaction_type {
+    raw[pointer] = value
+    pointer++
+  }
+  for _, value := range currency_id {
+    raw[pointer] = value
+    pointer++
+  }
+  for _, value := range amount {
+    raw[pointer] = value
+    pointer++
+  }
+
+  rawString := strings.Join(raw,"")
+
+  log.Debug("Raw string: ", rawString)
+
+  return rawString
+}
+
+// Encodes as Class A
+func (ss *SimpleSend) SerializeToAddress() string{
   log.Info("Encoding data to address")
 
   raw := make([]byte, 25)
--- simple_send_test.go
+++ simple_send_test.go
@@ -2,7 +2,51 @@ package main
 
 import ("testing"
         "bytes"
+        "strconv"
+        "reflect"
 )
+
+func TestSerializeToKey(t *testing.T){
+  // TODO: Verify values are the same as ruby implementation
+  tests := []struct {
+    address string
+    values SimpleSend
+  }{
+    {"01000000000000000200000000000000320000000000000000000000000000", SimpleSend{currency_id: 2, sequence: 76, transaction_type: 0, amount: 50}},
+    {"010000000000000002000000000000305e0000000000000000000000000000", SimpleSend{currency_id: 2, sequence: 48, transaction_type: 0, amount: 12382}},
+    {"0100000000000000010000000000018fee0000000000000000000000000000", SimpleSend{currency_id: 1, sequence: 48, transaction_type: 0, amount: 102382}},
+  }
+
+  for _, pair := range tests{
+    v := pair.values.SerializeToKey()
+    if v != pair.address {
+      t.Error("For", pair.address,
+              "Expected", pair.address,
+              "Got", v,
+      )
+
+    }
+  }
+}
+func TestMakeStringArray(t *testing.T){
+  tests := []struct {
+    value []string
+    expected []string
+  }{
+    {[]string{"100","4"}, []string{"0","1","0","0"}},
+    {[]string{"3292","8"}, []string{"0","0","0","0","3","2","9","2"}},
+  }
+  for _, pair := range tests {
+    toint,_ := strconv.Atoi(pair.value[1])
+    v := makeStringArray(pair.value[0], toint)
+    // TODO: DeepEqual is pretty slow. If we end up needing this more, make something custom.
+    if !reflect.DeepEqual(v, pair.expected) {
+      t.Error("Expected",pair.expected ,
+      "For", pair.value,
+      "Got", v)
+    }
+  }
+}
 
 func TestMakeBinary(t *testing.T){
   tests := []struct {
@@ -80,7 +124,7 @@ func TestEncodeToAddress(t *testing.T){
   }
 
   for _, pair := range tests{
-    v := pair.values.EncodeToAddress()
+    v := pair.values.SerializeToAddress()
     if v != pair.address {
       t.Error("For", pair.address,
               "Expected", pair.address,
